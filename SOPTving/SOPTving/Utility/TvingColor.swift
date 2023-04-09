ee973d94105d68b904678aaf00117a86bc529d17
Melt <seokwoo2000@naver.com>
2023-04-10 04:18:37 +0900

    [Set] Pretendard 폰트, 색상 추가 #2

---
+++ SOPTving/SOPTving/Utility/TvingColor.swift
@@ -0,0 +1,19 @@
+//
+//  TvingColor.swift
+//  SOPTving
+//
+//  Created by 장석우 on 2023/04/10.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    class var tvingRed: UIColor {
+        return UIColor(red: 234, green: 51, blue: 58, alpha: 1)
+    }
+    
+    
+    
+    
+}
